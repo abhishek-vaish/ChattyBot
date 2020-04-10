73304d598b54b345e2bec9b628d3155b2fe08607
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-04-10 10:40:05 +0530

    create parameterized constructor

--- src/pizza/PizzaClass.java
+++ src/pizza/PizzaClass.java
@@ -14,5 +14,10 @@ public class PizzaClass {
     String flavour;
     boolean offer;
 
-
+    public PizzaClass(String pizzaName, int prize, String flavour, boolean offer) {
+        this.pizzaName = pizzaName;
+        this.prize = prize;
+        this.flavour = flavour;
+        this.offer = offer;
+    }
 }
