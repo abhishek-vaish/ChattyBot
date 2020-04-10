68f52df3bc87a221e81ab8b8770c71aa02140116
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-04-10 12:10:37 +0530

    javadoc is added

--- src/linkedList/LinkedListDefination.java
+++ src/linkedList/LinkedListDefination.java
@@ -9,9 +9,15 @@
 package linkedList;
 
 public class LinkedListDefination<Pizza> implements LinkedListADT<Pizza> {
-    private int size = 0;
-    private Node<Pizza> head = null;
+    private int size = 0; //size of the pizza list
+    private Node<Pizza> head = null; //first pizza in the list
 
+    /**
+     * To convert the pizza number into the Node type
+     *
+     * @param index pizza number that user choose
+     * @return node type of the pizza choose by the user
+     */
     private Node<Pizza> getNode(int index) {
         Node<Pizza> response = head;
         for (int i = 0; i < index; i++) {
@@ -20,6 +26,11 @@ public class LinkedListDefination<Pizza> implements LinkedListADT<Pizza> {
         return response;
     }
 
+    /**
+     * Helping method for adding the pizza at the first place
+     *
+     * @param data pizza that have to be added at first place
+     */
     private void addFirst(Pizza data) {
         if (head == null) {
             head = new Node(data, null);
@@ -27,11 +38,23 @@ public class LinkedListDefination<Pizza> implements LinkedListADT<Pizza> {
         }
     }
 
+    /**
+     * Helping method for adding the pizza at the other than the first place
+     *
+     * @param data pizza that have to be added in the list
+     * @param node reference of the next pizza node that is inserted in the list
+     */
     private void addAfter(Pizza data, Node<Pizza> node) {
         node.next = new Node<>(data, node.next);
         size++;
     }
 
+    /**
+     * Helping overloaded method for adding the pizza int the list
+     *
+     * @param data  pizza that have to be added in the list
+     * @param index index number of the pizza that user choose
+     */
     private void add(Pizza data, int index) {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException(Integer.toString(index));
@@ -43,11 +66,21 @@ public class LinkedListDefination<Pizza> implements LinkedListADT<Pizza> {
         }
     }
 
+    /**
+     * To add the pizza into the order list
+     *
+     * @param data add the Pizza type data to the list that user want to order
+     */
     @Override
     public void add(Pizza data) {
         add(data, size);
     }
 
+    /**
+     * Helping method for deleting the pizza at the first place
+     *
+     * @return delete the pizza from the first place in the list
+     */
     private boolean deleteFirst() {
         boolean response = false;
         Node<Pizza> temp = head;
@@ -61,6 +94,12 @@ public class LinkedListDefination<Pizza> implements LinkedListADT<Pizza> {
         return response;
     }
 
+    /**
+     * Helping method for adding the pizza at the other than the first place
+     *
+     * @param node reference of the next pizza node that is deleted in the list
+     * @return delete the pizza from the first place in the list
+     */
     private boolean deleteAfter(Node<Pizza> node) {
         boolean response = false;
         Node<Pizza> temp = node.getNext();
@@ -71,6 +110,12 @@ public class LinkedListDefination<Pizza> implements LinkedListADT<Pizza> {
         return response;
     }
 
+    /**
+     * To delete the pizza from the list that user doesn't want to order
+     *
+     * @param index choose the pizza number from the order list for cancel the order
+     * @return if the order is cancel successfully than return true else false
+     */
     @Override
     public boolean delete(int index) {
         boolean response = false;
