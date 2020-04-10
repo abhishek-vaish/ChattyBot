5e3bd1d4d23288ebbf16d4cbf684e100c6d67ad1
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-04-10 11:19:59 +0530

    javadoc is added

--- src/linkedList/LinkedListADT.java
+++ src/linkedList/LinkedListADT.java
@@ -9,10 +9,25 @@
 package linkedList;
 
 public interface LinkedListADT<Pizza> {
+
+    /**
+     * To add the pizza into the order list
+     *
+     * @param data add the Pizza type data to the list that user want to order
+     */
     void add(Pizza data);
 
+    /**
+     * To delete the pizza from the list that user doesn't want to order
+     *
+     * @param index choose the pizza number from the order list for cancel the order
+     * @return if the order is cancel successfully than return true else false
+     */
     boolean delete(int index);
 
+    /**
+     * To display the list of pizza that user ordered
+     */
     void display();
 
 }
