a73d9ba62e6db5c911f2a89814151da1853bb51b
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-04-10 10:46:13 +0530

    create chatAoperation and implement ChatAdt method

---
+++ src/chatOperation/ChatOperation.java
@@ -0,0 +1,27 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: Abhishek
+ *   Date: 09-04-2020
+ *   Time: 08:10 PM
+ */
+
+
+package chatOperation;
+
+public class ChatOperation<Pizza> implements ChatADT<Pizza> {
+
+    @Override
+    public void order(int index) {
+
+    }
+
+    @Override
+    public void reorder(int index) {
+
+    }
+
+    @Override
+    public boolean payment(int pizzaPrice) {
+        return false;
+    }
+}
