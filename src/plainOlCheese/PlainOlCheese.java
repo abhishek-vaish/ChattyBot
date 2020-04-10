15700a4c6e481bdb89d29d16159807d0513d9d8e
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-04-10 10:44:14 +0530

    create theBigPepperoni method

--- src/plainOlCheese/PlainOlCheese.java
+++ src/plainOlCheese/PlainOlCheese.java
@@ -8,9 +8,25 @@
 
 package plainOlCheese;
 
+import pizza.PizzaClass;
+
 public class PlainOlCheese {
     String pizzaName;
     int prize;
     String flavour;
     boolean offer;
+
+    public String theBigPepperoni() {
+        PizzaClass pizzaClass = new PizzaClass();
+        pizzaName = "THE BIG PEPPERONI";
+        prize = 400;
+        flavour = "Plain OI' Cheese";
+        offer = false;
+        pizzaClass.setPizzaName(pizzaName);
+        pizzaClass.setFlavour(flavour);
+        pizzaClass.setOffer(offer);
+        pizzaClass.setPrize(prize);
+        return pizzaClass.toString();
+    }
+
 }
