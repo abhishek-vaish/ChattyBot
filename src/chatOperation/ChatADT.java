0c3f717d26224ff3941abd309e7b5f6d5d70d9f9
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-04-10 11:10:22 +0530

    javadoc is added

--- src/chatOperation/ChatADT.java
+++ src/chatOperation/ChatADT.java
@@ -1,10 +1,25 @@
 package chatOperation;
 
 public interface ChatADT<Pizza> {
+    /**
+     * For order the pizza from the list
+     *
+     * @param index choose the pizza number from the list and give it to order method as a parameter
+     */
     void order(int index);
 
+    /**
+     * If the customer mood change and reorder the pizza and cancel the old one
+     *
+     * @param index choose the pizza number from the list and give it to order method as a parameter
+     */
     void reorder(int index);
 
+    /**
+     * For the Pizza payment if the given amount is equal to pizza prize then only the pizza is order
+     *
+     * @param pizzaPrice amount that user gives to order the pizza
+     */
     boolean payment(int pizzaPrice);
 
 }
