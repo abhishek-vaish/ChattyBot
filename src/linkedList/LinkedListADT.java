9a199abb6afc27f7c064a908ceb6a2877aa46159
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-04-10 10:23:47 +0530

    LinkedListADT created

---
+++ src/linkedList/LinkedListADT.java
@@ -0,0 +1,18 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: Abhishek
+ *   Date: 09-04-2020
+ *   Time: 08:20 PM
+ */
+
+
+package linkedList;
+
+public interface LinkedListADT<Pizza> {
+    void add(Pizza data);
+
+    boolean delete(int index);
+
+    void display();
+
+}
