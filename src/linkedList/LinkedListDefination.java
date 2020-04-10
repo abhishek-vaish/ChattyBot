1997d885c2ecd31e598f02a54e0290820276ec4b
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-04-10 10:26:46 +0530

    LinkedListDefination class created and implements with LinkedListADT and its method

---
+++ src/linkedList/LinkedListDefination.java
@@ -0,0 +1,28 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: Abhishek
+ *   Date: 09-04-2020
+ *   Time: 08:20 PM
+ */
+
+
+package linkedList;
+
+public class LinkedListDefination<Pizza> implements LinkedListADT<Pizza> {
+
+
+    @Override
+    public void add(Pizza data) {
+
+    }
+
+    @Override
+    public boolean delete(int index) {
+        return false;
+    }
+
+    @Override
+    public void display() {
+
+    }
+}
