c6a0b959850f0881fee93e023d1c1727e1f405d1
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-04-10 10:42:45 +0530

    create PlainOlCheese class

---
+++ src/plainOlCheese/PlainOlCheese.java
@@ -0,0 +1,13 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: Abhishek
+ *   Date: 09-04-2020
+ *   Time: 09:31 PM
+ */
+
+
+package plainOlCheese;
+
+public class PlainOlCheese {
+
+}
