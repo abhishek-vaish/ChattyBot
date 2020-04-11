1424248011015e6e33f3ce47359b957b3cd74cf1
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-04-11 16:34:24 +0530

    menuCard is created for the user

---
+++ src/menuCard/MenuCard.java
@@ -0,0 +1,85 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: Abhishek
+ *   Date: 09-04-2020
+ *   Time: 09:31 PM
+ */
+
+
+package menuCard;
+
+public class MenuCard {
+    public void menuList() {
+        System.out.println("Food\tSize\tPrice\n" +
+                "Cheese Pizza\n" +
+                "Hand-Tossed\t\tMedium\t\t$11.99\n" +
+                "Hand-Tossed\t\tLarge\t\t$14.99\n" +
+                "Pan Pizza\t\tPersonal\t\t$4.79\n" +
+                "Pan Pizza\t\tMedium\t\t$11.99\n" +
+                "Pan Pizza\t\tLarge\t\t$14.99\n" +
+                "Thin N Crispy\t\tMedium\t\t$11.99\n" +
+                "Thin N Crispy\t\tLarge\t\t$14.99\n" +
+                "Stuffed Crust\t\tLarge\t\t$16.99\n" +
+                "Skinny Slice\t\tLarge\t\t$14.99\n\n" +
+                "Classic Recipe Pizzas\n" +
+                "Pepperoni Lover's® Pizza (Hand-Tossed)\t\tMedium\t\t$11.99\n" +
+                "Pepperoni Lover's® Pizza (Hand-Tossed)\t\tLarge\t\t$14.99\n" +
+                "Pepperoni Lover's® Pizza (Pan Pizza)\t\tPersonal\t\t$4.79\n" +
+                "Pepperoni Lover's® Pizza (Pan Pizza)\t\tMedium\t\t$11.99\n" +
+                "Pepperoni Lover's® Pizza (Pan Pizza)\t\tLarge\t\t$14.99\n" +
+                "Meat Lover's® Pizza (Hand-Tossed)\t\tMedium\t\t$11.99\n" +
+                "Meat Lover's® Pizza (Hand-Tossed)\t\tLarge\t\t$14.99\n" +
+                "Meat Lover's® Pizza (Pan Pizza)\t\tPersonal\t\t$4.79\n" +
+                "Meat Lover's® Pizza (Pan Pizza)\t\tMedium\t\t$11.99\n" +
+                "Meat Lover's® Pizza (Pan Pizza)\t\tLarge\t\t$14.99\n" +
+                "Meat Lover's® Pizza (Thin N Crispy)\t\tMedium\t\t$11.99\n" +
+                "Meat Lover's® Pizza (Thin N Crispy)\t\tLarge\t\t$14.99\n" +
+                "Meat Lover's® Pizza (Stuffed Crust)\t\tLarge\t\t$16.99\n" +
+                "Meat Lover's® Pizza (Skinny Slice)\t\tLarge\t\t$14.99\n" +
+                "Veggie Lover's® Pizza (Hand-Tossed)\t\tMedium\t\t$11.99\n" +
+                "Veggie Lover's® Pizza (Hand-Tossed)\t\tLarge\t\t$14.99\n" +
+                "Veggie Lover's® Pizza (Pan Pizza)\t\tPersonal\t\t$4.79\n" +
+                "Veggie Lover's® Pizza (Pan Pizza)\t\tMedium\t\t$11.99\n" +
+                "Veggie Lover's® Pizza (Pan Pizza)\t\tLarge\t\t$14.99\n" +
+                "Supreme Pizza (Hand-Tossed)\t\tMedium\t\t$11.99\n" +
+                "Supreme Pizza (Hand-Tossed)\t\tLarge\t\t$14.99\n" +
+                "Supreme Pizza (Pan Pizza)\t\tPersonal\t\t$4.79\n" +
+                "Supreme Pizza (Pan Pizza)\t\tMedium\t\t$11.99\n" +
+                "Supreme Pizza (Pan Pizza)\t\tLarge\t\t$14.99\n" +
+                "Supreme Pizza (Thin N Crispy)\t\tMedium\t\t$11.99\n" +
+                "Supreme Pizza (Thin N Crispy)\t\tLarge\t\t$14.99\n" +
+                "Supreme Pizza (Stuffed Crust)\t\tLarge\t\t$16.99\n" +
+                "Supreme Pizza (Skinny Slice)\t\tLarge\t\t$14.99\n" +
+                "BBQ Lover's™ (Hand-Tossed)\t\tMedium\t\t$11.99\n" +
+                "BBQ Lover's™ (Hand-Tossed)\t\tLarge\t\t$14.99\n" +
+                "BBQ Lover's™ (Pan Pizza)\t\tPersonal\t\t$4.79\n" +
+                "BBQ Lover's™ (Pan Pizza)\t\tMedium\t\t$11.99\n" +
+                "BBQ Lover's™ (Pan Pizza)\t\tLarge\t\t$14.99\n" +
+                "BBQ Lover's™ (Thin N Crispy)\t\tMedium\t\t$11.99\n" +
+                "BBQ Lover's™ (Thin N Crispy)\t\tLarge\t\t$14.99\n" +
+                "BBQ Lover's™ (Stuffed Crust)\t\tLarge\t\t$16.99\n" +
+                "BBQ Lover's™ (Skinny Slice)\t\tLarge\t\t$14.99\n\n" +
+                "Sauces\n" +
+                "Blue Cheese\t\t$0.60\n" +
+                "Chocolate\t\t$0.60\n" +
+                "Ranch\t\t$0.60\n" +
+                "Garlic\t\t$0.60\n" +
+                "Marinara\t\t$0.60\n" +
+                "Icing\t\t$0.60\n" +
+                "Classic Sauce Set\t\t4 Pc.\t\t$2.00\n" +
+                "Explore Sauce Set\t\t5 Pc.\t\t$2.50\n\n" +
+                "Extras\n" +
+                "Side of Banana Peppers\t\t$1.00\n" +
+                "Parmesan Cheese Packet\t\tFree\n" +
+                "Red Pepper Packet\t\tFree\n\n" +
+                "Drinks\n" +
+                "Pepsi®\t\tBottle\t\t$1.79\n" +
+                "Pepsi®\t\t2 Liter\t\t$2.99\n" +
+                "Diet Pepsi®\t\tBottle\t\t$1.79\n" +
+                "Diet Pepsi®\t\t2 Liter\t\t$2.99\n" +
+                "Mountain Dew®\t\tBottle\t\t$1.79\n" +
+                "Mountain Dew®\t\t2 Liter\t\t$2.99\n" +
+                "Aquafina\t\tBottle\t\t$1.49\n" +
+                "Dr. Pepper®\t\t2 Liter\t\t$2.99");
+    }
+}
